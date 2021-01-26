c862cda589bcfceba90b3ca75f61983d59d71675
Mehdi Oueslati <mehdi.oueslati@epita.fr>
2021-01-26 21:16:06 +0100

    added the return calling self.find_simplifiers

--- genus/src/s_combination.py
+++ genus/src/s_combination.py
@@ -124,6 +124,8 @@ class SCombination(SimpleTypeD):
 			#implement when SNot is done
 			raise NotImplementedError
 
+		simplifiers = [l_1, l_2, l_3, l_4, l_5, l_6, l_7, l_8]
+		return self.find_simplifier(simplifiers)
 
 	def cmp_to_same_class_obj(self, td):
 		#TODO: check this, this is is probably wrong
