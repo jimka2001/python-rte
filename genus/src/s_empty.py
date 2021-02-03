af60128a9b367d535c5975e05c985eaefd7040fa
Mehdi Oueslati <mehdi.oueslati@epita.fr>
2021-02-03 01:55:49 +0100

    s_empty is now unique too

--- genus/src/s_empty.py
+++ genus/src/s_empty.py
@@ -22,53 +22,79 @@
 from simple_type_d import SimpleTypeD 
 
 class SEmpty(SimpleTypeD):
-	"""The empty type, subtype of all types."""
-	def __init__(self):
-		super(SEmpty, self).__init__()
+    """The empty type, subtype of all types."""
+    __instance = None
 
-	def __str__(self):
-		return "Empty"
+    @staticmethod
+    def get_omega():
+        if SEmpty.__instance == None:
+            SEmpty()
+        return SEmpty.__instance
 
-	def typep(self, any):
-		return False
+    def __init__(self):
+        super(SEmpty, self).__init__()
+        if SEmpty.__instance != None:
+           raise Exception("Please use SEmpty.get_omega() as SEmpty is unique and can't be duplicated")
+        else:
+           SEmpty.__instance = self
 
-	def _inhabited_down(self):
-		return False
+    def __str__(self):
+        return "Empty"
 
-	def _disjoint_down(self, t):
-		return True
+    def typep(self, any):
+        return False
 
-	def subtypep(self, t):
-		return True
+    def _inhabited_down(self):
+        return False
 
-	def cmp_to_same_class_obj(self, t):
-		return False
+    def _disjoint_down(self, t):
+        return True
+
+    def subtypep(self, t):
+        return True
+
+    def cmp_to_same_class_obj(self, t):
+        return False
 
 #TODO: move the tests in their own files once this is packaged:
 def t_SEmpty():
-	a = SEmpty() #TODO: make this class uninstantiable and provide one single instance
-	
-	#str(a) has to be "Empty"
-	assert(str(a) == "Empty")
+    a = SEmpty.get_omega()
 
-	#a.typep(t) indicates whether t is a subtype of a, which is never the case
-	assert(not a.typep(object))
-	assert(not a.typep(a))
+    #SEmpty has to be unique
+    assert(id(a) == id(SEmpty.get_omega()))
+    
+    #SEmpty has to be unique part 2: ensure the constructor throws an error
+    pred = True
+    try:
+        b = SEmpty()
+        pred = False
+        assert(False)
+    except Exception as e:
+        assert(pred)
 
-	#obviously, a is not inhabited as it is by definition empty
-	assert(not a._inhabited_down)
+    #str(a) has to be "Empty"
+    assert(str(a) == "Empty")
 
-	#a is disjoint with anything as it is empty
-	assert(a._disjoint_down(object))
+    #a.typep(t) indicates whether t is a subtype of a, which is never the case
+    assert(not a.typep(object))
+    assert(not a.typep(a))
 
-	#on the contrary, a is always a subtype of any type 
-	#since types are sets and the empty set is a subset of all sets
-	assert(a.subtypep(object))
-	assert(a.subtypep(type(a)))
+    #obviously, a is not inhabited as it is by definition empty
+    assert(not a._inhabited_down())
 
-	#my understanding is that the empty type is unique so it can't be positively compared to any object
-	assert(not a.cmp_to_same_class_obj(a))
-	assert(not a.cmp_to_same_class_obj(object))
+    #a is disjoint with anything as it is empty
+    assert(a._disjoint_down(object))
+
+    #on the contrary, a is always a subtype of any type 
+    #since types are sets and the empty set is a subset of all sets
+    assert(a.subtypep(object))
+    assert(a.subtypep(type(a)))
+
+    #my understanding is that the empty type is unique so it can't be positively compared to any object
+    assert(not a.cmp_to_same_class_obj(a))
+    assert(not a.cmp_to_same_class_obj(object))
+
+t_SEmpty()
 
 """ Scala code to print values used for testing:
 
