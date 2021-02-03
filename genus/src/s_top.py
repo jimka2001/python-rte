68479e37002a8faef84d4297aaa204da55f3192a
Mehdi Oueslati <mehdi.oueslati@epita.fr>
2021-02-03 01:47:08 +0100

    tried a bunch of workarounds to not having private constructors, ended up just going for a singleton implementation

--- genus/src/s_top.py
+++ genus/src/s_top.py
@@ -20,56 +20,85 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from simple_type_d import SimpleTypeD 
+from s_empty import SEmpty
 
+""" test-coverage as (method name, state[0-3] {0 not implemented, 1 implemented, 2 partially tested,  3 fully done})
+"""
 class STop(SimpleTypeD):
-	"""The super type, super type of all types."""
-	def __init__(self):
-		super(STop, self).__init__()
+    """The super type, super type of all types."""
+    __instance = None
 
-	def __str__(self):
-		return "Top"
+    @staticmethod
+    def get_omega():
+        if STop.__instance == None:
+            STop()
+        return STop.__instance
 
-	def typep(self, any):
-		return True
+    def __init__(self):
+        super(STop, self).__init__()
+        if STop.__instance != None:
+           raise Exception("Please use STop.get_omega() as STop is unique and can't be duplicated")
+        else:
+           STop.__instance = self
 
-	def _inhabited_down(self):
-		return True
+    def __str__(self):
+        return "Top"
 
-	def _disjoint_down(self, t):
-		return type(t) is SEmpty
+    def typep(self, any):
+        return True
 
-	def subtypep(self, t):
-		return type(t) == STop
+    def _inhabited_down(self):
+        return True
 
-	def cmp_to_same_class_obj(self, t):
-		return False
+    def _disjoint_down(self, t):
+        return type(t) is SEmpty
+
+    def subtypep(self, t):
+        return type(t) == STop
+
+    def cmp_to_same_class_obj(self, t):
+        return False
 
 #TODO: move the tests in their own files once this is packaged:
 def t_STop():
-	a = STop() #TODO: make this class uninstantiable and provide one single instance
-	
-	#str(a) has to be "Top"
-	assert(str(a) == "Top")
+    a = STop.get_omega()
 
-	#a.typep(t) indicates whether t is a subtype of a, which is always the case by definition
-	assert(a.typep(object))
-	assert(a.typep(a))
+    #STop has to be unique
+    assert(id(a) == id(STop.get_omega()))
+    
+    #STop has to be unique part 2: ensure the constructor throws an error
+    pred = True
+    try:
+        b = STop()
+        pred = False
+        assert(False)
+    except Exception as e:
+        assert(pred)
 
-	#obviously, a is inhabited as it contains everything by definition
-	assert(a._inhabited_down)
+    #str(a) has to be "Top"
+    assert(str(a) == "Top")
 
-	#a is never disjoint with anything but the empty subtype
-	assert(not a._disjoint_down(object))
-	assert(a._disjoint_down(SEmpty()))
+    #a.typep(t) indicates whether t is a subtype of a, which is always the case by definition
+    assert(a.typep(object))
+    assert(a.typep(a))
 
-	#on the contrary, a is never a subtype of any type 
-	#since types are sets and top is the set that contains all sets
-	assert(not a.subtypep(object))
-	assert(not a.subtypep(type(a)))
+    #obviously, a is inhabited as it contains everything by definition
+    assert(a._inhabited_down)
 
-	#my understanding is that the top type is unique so it can't be positively compared to any object
-	assert(not a.cmp_to_same_class_obj(a))
-	assert(not a.cmp_to_same_class_obj(object))
+    #a is never disjoint with anything but the empty subtype
+    assert(not a._disjoint_down(object))
+    assert(a._disjoint_down(SEmpty()))
+
+    #on the contrary, a is never a subtype of any type 
+    #since types are sets and top is the set that contains all sets
+    assert(not a.subtypep(object))
+    assert(not a.subtypep(type(a)))
+
+    #my understanding is that the top type is unique so it can't be positively compared to any object
+    assert(not a.cmp_to_same_class_obj(a))
+    assert(not a.cmp_to_same_class_obj(object))
+
+t_STop()
 
 """
 object t_STop {
