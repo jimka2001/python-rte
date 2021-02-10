871b94dda2a4032fd8642bb675f16848e0488eba
Mehdi OUESLATI <mehdi.oueslati@epita.fr>
2021-02-10 03:16:43 +0100

    created s_custom

---
+++ genus/src/s_custom.py
@@ -0,0 +1,32 @@
+# Copyright (©) 2021 EPITA Research and Development Laboratory
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+[0-3] Advancement tracker
+__init__ 0
+typep 0
+__str__ 0
+_disjoint_down 0
+_inhabited_down 0
+subtypep 0
+cmp_to_same_class_obj 0
+apply 0
+"""
