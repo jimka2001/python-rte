69dc9d4d94420707b8963bbba8fa1c5eeec008ee
Jim Newton <jnewton@Marcello.local>
2021-06-23 18:22:30 +0200

    white space

--- tests/all_tests.py
+++ tests/all_tests.py
@@ -710,20 +710,21 @@ def t_and_conversionA1():
     # to SAnd(SMember(42, 43, 44), SInt)
     # while conversionA1() converts it to
     # SMember(42, 43, 44)
-    assert SAnd(SMember(1,2,3,"a","b","c"),SAtomic(int)).conversionA1() == SMember(1,2,3)
+    assert SAnd(SMember(1, 2, 3, "a", "b", "c"), SAtomic(int)).conversionA1() == SMember(1, 2, 3)
 
 
 def t_or_conversionO1():
     # SOr(SNot(SMember(42, 43, 44, "a","b")), String)
     # == > SNot(SMember(42, 43, 44))
-    assert SOr(SNot(SMember(1,2,3,"a","b","c")), SAtomic(int)).conversionO1() == SNot(SMember("a","b","c"))
+    assert SOr(SNot(SMember(1, 2, 3, "a", "b", "c")), SAtomic(int)).conversionO1() == SNot(SMember("a", "b", "c"))
 
 
 def t_and_conversionA3():
     # find disjoint pair
-    assert SAnd(SMember(1,2),SMember(3,4)).conversionA3() == SEmpty
-    assert SAnd(SMember("a","b"),SAtomic(int)).conversionA3() == SEmpty
-    assert SAnd(SMember(1,2,"a","b"),SAtomic(int)).conversionA3() == SAnd(SMember(1,2,"a","b"),SAtomic(int))
+    assert SAnd(SMember(1, 2), SMember(3, 4)).conversionA3() == SEmpty
+    assert SAnd(SMember("a", "b"), SAtomic(int)).conversionA3() == SEmpty
+    assert SAnd(SMember(1, 2, "a", "b"), SAtomic(int)).conversionA3() == SAnd(SMember(1, 2, "a", "b"), SAtomic(int))
+
 
 #   calling the test functions
 
