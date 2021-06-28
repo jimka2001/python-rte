740a27a3721f6cec95262c2abb3a71c8d733bbdb
Jim Newton <jnewton@Marcello.local>
2021-06-28 13:09:44 +0200

    lint issue

--- genus/src/s_member.py
+++ genus/src/s_member.py
@@ -66,7 +66,7 @@ class SMemberImpl(SimpleTypeD):
 	def subtypep_down(self, t2):
 		return all(t2.typep(a) for a in self.arglist)
 
-	def canonicalize_once(self, _nf):
+	def canonicalize_once(self, _nf=None):
 		return self
 
 	def cmp_to_same_class_obj(self, t):
